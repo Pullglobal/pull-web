346e3485aee97b56d74e94e369ac763974d196b1
Isaiah Lewis <isaiahlewis@Isaiahs-MacBook-Pro.local>
2026-05-27 23:54:47 -0400

    light map, zero price, search pin

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -147,7 +147,7 @@ export default function MapPage() {
         filter: ['==', 'extrude', 'true'],
         type: 'fill-extrusion',
         paint: {
-          'fill-extrusion-color': '#e6c20e',
+          'fill-extrusion-color': '#105b36a8',
           'fill-extrusion-height': ['get', 'height'],
           'fill-extrusion-base': ['get', 'min_height'],
           'fill-extrusion-opacity': 0.7,
